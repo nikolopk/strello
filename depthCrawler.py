8ea757100d067c5b22d7b791bfaa5d8042e9dc93
nikolopk <kosnikolopoulos@hotmail.com>
2017-10-04 21:44:30 +0300

    rss4

---
+++ depthCrawler.py
@@ -0,0 +1,18 @@
+import urllib2
+import lxml.html
+
+
+def crawl(url = 'http://www.bbc.com/news', depth=3):
+    if depth == 0:
+        return None
+    try:
+        page = urllib2.urlopen(url)
+    except (urllib2.URLError, ValueError):
+        return None
+
+    html = page.read()
+    dom = lxml.html.fromstring(html)
+
+    print "level %d: %s" % (depth, url)
+    for link in dom.xpath('//a/@href'):
+        crawl(link, depth - 1)
